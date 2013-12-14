1164daf4b384d4080ebb6a95221e5c90effc0948
Tom Gurion <nagasaki45@gmail.com>
2013-12-14 19:26:56 +0200

    Starting to implement 'scrap_zone' function to scraper.py. Unfinished

--- openair/records/scraper.py
+++ openair/records/scraper.py
@@ -6,14 +6,16 @@ python scraper.py <url_id>
 
 Functions
 ---------
-scrap(url_id) -> records
+scrap_station(url_id) -> records
+scrap_zone(url_id) -> records
 '''
 
 import sys
 import urllib2
 from bs4 import BeautifulSoup
 
-def scrap(url_id):
+
+def scrap_station(url_id):
     '''
     Scrap air parameters from
     http://www.svivaaqm.net/Online.aspx?ST_ID=<STATION_ID>;0
@@ -57,13 +59,70 @@ def scrap(url_id):
     return records
 
 
-if __name__ == '__main__':
-    if len(sys.argv) > 0:
+def scrap_zone(url_id):
+    '''
+    Scrap air parameters from
+    http://www.svivaaqm.net/DynamicTable.aspx?G_ID=<ZONE_ID>
+
+    Parameters
+    ----------
+    url_id : int
+        Replaces <ZONE_ID> in the url.
+
+    Returns
+    -------
+    records : dict
+        Dictionary of station:dict pairs. Stations are the url_id
+        of the station and the dict are the records as received by
+        scrap_station() function.
+    '''
+
+    url ='http://www.svivaaqm.net/DynamicTable.aspx?G_ID={}' \
+        .format(url_id)
+
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    response = opener.open(url)
+    soup = BeautifulSoup(response.read())
+    table = soup.find('table',
+                       id='C1WebGrid1')
+
+    # the first row in the table is for abbreviations
+    abbreviations = []
+    for element in table.find('tr'):
+        for cell in element:
+            try:
+                abbreviations.append(cell.get_text().strip('\r\n\t'))
+            except Exception, e:
+                pass
+
+    # TODO COMPLETE THE PARSER
+    # records = {}
+    # for element in table.find_all('tr')[2:]:
+    #     station_records = {}
+    #     abbr = element.a.string.split('[')[0]
+    #     try:
+    #         value = float(element.b.string)
+    #     except (ValueError):
+    #         value = None
+
+    #     records[abbr] = value
+
+    # return records
+
+
+def main():
+    if len(sys.argv) > 1:
         url_id = sys.argv[1]
+        print('Showing records for station with url_id = {}' \
+            .format(url_id))
+        results = scrap(url_id)
+        for abbr in results.keys():
+            print('{}: {}'.format(abbr, results[abbr]))
     else:
-        url_id = 11  # just for example
-    print('Showing records for station with url_id = {}' \
-        .format(url_id))
-    results = scrap(url_id)
-    for abbr in results.keys():
-        print('{}: {}'.format(abbr, results[abbr]))
+        print('Type "station" or "zone" and url_id')
+
+
+if __name__ == '__main__':
+    # main()
+    scrap_zone(8)
--- openair/settings.py
+++ openair/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rd$$-%b+mt!-&guburi8+e)0$(bt@*-gt4dh(pc4wly(4ervgx'
+SECRET_KEY = os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
