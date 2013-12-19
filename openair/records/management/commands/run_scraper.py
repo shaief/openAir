e371a55ae135dc39f11a3fff963f059f5cf0f6b2
Tom Gurion <nagasaki45@gmail.com>
2013-12-19 23:40:50 +0200

    Implementing django command to scrape the data and fill the DB

---
+++ openair/records/management/commands/run_scraper.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+from django.core.management.base import BaseCommand, CommandError
+from openair.records.models import Zone, Station, Parameter, Record
+from openair.records.scraper import scrape_zone
+
+
+class Command(BaseCommand):
+    help = 'Run the scraper on the zones to fill the DB'
+
+
+    def handle(self, *args, **options):
+        
+        # run over all of the zones
+        for zone in Zone.objects.all():
+
+            results = scrape_zone(zone.url_id)
+
+            # run over the received stations
+            for station_url_id in results.keys():
+
+                station, success = Station.objects.get_or_create(
+                    url_id=station_url_id,
+                    zone=zone
+                )
+
+                timestamp = datetime.strptime(
+                    results[station_url_id].pop('timestamp'),
+                    '%d/%m/%Y %H:%M'
+                )
+
+                for abbr in results[station_url_id].keys():
+
+                    # don't process this value if there is none
+                    if results[station_url_id][abbr] is None:
+                        continue
+
+                    parameter, success = Parameter \
+                        .objects.get_or_create(abbr=abbr)
+
+                    # don't create records if there are already there
+                    record, success = Record.objects.get_or_create(
+                        station=station,
+                        parameter=parameter,
+                        value=results[station_url_id][abbr],
+                        timestamp=timestamp
+                    )
+
+                    record.save()
