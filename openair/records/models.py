92c0a4ac096019eaed288f555db956d19780779d
oz123 <nahumoz@gmail.com>
2013-12-24 11:03:48 +0100

    add helper function to get record by dates

--- openair/records/models.py
+++ openair/records/models.py
@@ -1,7 +1,27 @@
 from django.db import models
 import pytz
 from openair.settings.base import TIME_ZONE
+from django.db.models import Q
 
+# The following code probably should not stay here ...
+# But for now it's a scratch pad here.
+
+def get_param_time_range(rec, begin, end):
+    """
+    # get all the measurments from a parameter
+    # for a specific time range
+    # Here is an example how to query DB for all the measurments
+    # of 2014, not filtered by a station
+    
+    >>> start = datetime.date(2013, 1, 1)
+    >>> end = datetime.date(2014, 1, 1)
+    >>> rec = Parameter.objects.first()
+    >>> 2013_measures = get_param_time_range(start, end)
+    """
+    
+    Q(timestamp__range=[begin, end])
+    return rec.record_set.filter(Q)
+    
 
 class Zone(models.Model):
     url_id = models.IntegerField()
@@ -28,7 +48,7 @@ class Parameter(models.Model):
 
     def __unicode__(self):  # Python 3: def __str__(self):
         return self.abbr
-
+    
 
 class Record(models.Model):
     parameter = models.ForeignKey(Parameter)
