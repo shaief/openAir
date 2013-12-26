531273347654488c780d7c0e61fd69f24cc023cb
oz123 <nahumoz@gmail.com>
2013-12-26 14:27:52 +0100

    fix view and template to plot measurements

--- openair/records/views.py
+++ openair/records/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404, render_to_response
-from django.utils import simplejson
-from openair.records.models import Record, Station
+# from django.utils import simplejson
+import json
+import datetime
+from openair.records.models import (Record, Parameter, Station, 
+                                    get_param_time_range)
 
 
 # def parameters(request):
@@ -35,11 +38,17 @@ def my_ajax_view(request):
     if not request.is_ajax():
         raise Http404
     data_dict = getmydata() #let's suppose is a dict
-    return HttpResponse(simplejson.dumps(data_dict))
+    return HttpResponse(json.dumps(data_dict))
 '''
 
 
 def station(request, station_id, start, end):
     # accessible over: station-1-2013-to-2014
-    context = {}
+    start = datetime.date(2013, 1, 1)
+    end = datetime.date(2014, 1, 1)
+    wd = Parameter.objects.first()
+    wd_rec = get_param_time_range(wd, start, end)
+    tstamps, measurements = zip(*[(x.timestamp, x.value) for x in wd_rec])
+    measurements = '['+', '.join([str(x) for x in measurements])+']'
+    context = {"measurements": measurements}
     return render(request, 'records/station_view.html', context)
