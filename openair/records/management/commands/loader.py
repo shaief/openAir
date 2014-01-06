c214f4e01b79f88cb245253c8fd772d95a41966a
oz123 <nahumoz@gmail.com>
2014-01-06 14:00:27 +0100

    make loader script a django command

--- openair/records/management/commands/loader.py
+++ openair/records/management/commands/loader.py
@@ -2,11 +2,11 @@
 """
 script to load old data from svivaaqm.net
 
-Health Warning :-/ Stinky code ahead of you. Read on your own risk. 
-The code is still not documented, does and not comply to pep8, hence 
-it stinks. 
+Health Warning :-/ Stinky code ahead of you. Read on your own risk.
+The code is still not documented, does and not comply to pep8, hence
+it stinks.
 
-It is a work in progress. 
+It is a work in progress.
 
 
 """
@@ -14,7 +14,16 @@ import pandas as pd
 import pytz
 from openair.settings.base import TIME_ZONE
 from openair.records.models import Zone, Station, Parameter, Record
+from django.core.management.base import BaseCommand, CommandError
 
+
+class Command(BaseCommand):
+    args = '<url_id> <station_csv>'
+    help = 'Add data from file station_csv to database, the record given'\
+        + ' in the file are created with url_id as station identifier'
+
+    def handle(self, *args, **options):
+        pass
 #record, success = Record.objects.get_or_create(
 #                        station=station,
 #                        parameter=parameter,
@@ -24,15 +33,16 @@ from openair.records.models import Zone, Station, Parameter, Record
 # station url_id should be given as cli parameter
 
 # STATION = url_id
-STATION = Station.objects.get(url_id=1)
+#STATION = Station.objects.get(url_id=1)
 
 
-a = pd.read_csv('openair/StationData1.csv', index_col=[20])
+#a = pd.read_csv('openair/StationData1.csv', index_col=[20])
 
-for item in in a.columns
-    param = getattr(a, item)
-    for date, val in nox.iteritems():
-        Record.objects.get_or_create(station=station,
-                                     parameter=item,
-                                     value=val, 
-                                     timestamp=date)
+
+#for item in a.columns:
+#    param = getattr(a, item)
+#    for date, val in param.iteritems():
+#        Record.objects.get_or_create(station=STATION,
+#                                     parameter=item,
+#                                     value=val,
+#                                     timestamp=date)
