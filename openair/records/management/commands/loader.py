8ff3a20e5853c07998cb74a501203f8548c6eb5e
oz123 <nahumoz@gmail.com>
2014-01-06 22:24:38 +0100

    some re-structuring of loader command

--- openair/records/management/commands/loader.py
+++ openair/records/management/commands/loader.py
@@ -12,8 +12,9 @@ It is a work in progress.
 """
 import pandas as pd
 import pytz
+from datetime import datetime
 from openair.settings.base import TIME_ZONE
-from openair.records.models import Zone, Station, Parameter, Record
+from openair.records.models import Station, Record
 from django.core.management.base import BaseCommand, CommandError
 
 
@@ -23,10 +24,27 @@ class Command(BaseCommand):
         + ' in the file are created with url_id as station identifier'
 
     def handle(self, *args, **options):
-        csv_file = args[0]
-        url_id = args[1]
+        csv_file, url_id = args[0], arg[1]
         print url_id, '  ', csv_file
+        station = Station.objects.get(url_id=url_id)
         data = pd.read_csv(csv_file, index_col=[20])
+        local = pytz.timezone(TIME_ZONE)
+
+        for item in data.columns:
+            param = getattr(data, item)
+            for date, value in param.itemitems():
+
+                # the hour 24:00 is not allowed
+                date = date.replace(' 24:', ' 00:')
+                naive_timestamp = datetime.strptime(date, '%d/%m/%Y %H:%M')
+                timestamp = local.localize(naive_timestamp)
+
+                #Record.objects.get_or_create(
+                #        station=station,
+                #        parameter=value,
+                #        value=results[station_url_id][abbr],
+                #        timestamp=timestamp
+                #        )
 
 #record, success = Record.objects.get_or_create(
 #                        station=station,
@@ -35,9 +53,6 @@ class Command(BaseCommand):
 #                        timestamp=timestamp
 #                    )
 # station url_id should be given as cli parameter
-
-# STATION = url_id
-#STATION = Station.objects.get(url_id=1)
 
 
 #a = pd.read_csv('openair/StationData1.csv', index_col=[20])
