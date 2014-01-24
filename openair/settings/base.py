3932055d41f54f57324e730332c0f347db2f5725
shaief <shaief@gmail.com>
2014-01-24 18:32:58 +0200

    postgres fix - host

--- openair/settings/base.py
+++ openair/settings/base.py
@@ -152,7 +152,7 @@ DATABASES = {
             # The following settings are not used with sqlite3:
             'USER': 'openair',
             'PASSWORD': 'tuuhrp,uj',
-            'HOST': 'localhost',                      # Empty for localhost through domain sockets or $
-            'PORT': '',                      # Set to empty string for default.
+            'HOST': 'http://188.226.133.47/',     # Empty for localhost through domain sockets or $
+            'PORT': '',              # Set to empty string for default.
         }
     }
