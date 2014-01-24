09dac51173c93ac94ed40efb9f77c188b21d18ff
shaief <shaief@gmail.com>
2014-01-24 18:56:44 +0200

    ...moving from sqllite to postgres

--- openair/settings/local.py
+++ openair/settings/local.py
@@ -6,9 +6,22 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': PROJECT_DIR.child('db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', $
+            'NAME': 'openair',                      # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': 'openair',
+            'PASSWORD': 'tuuhrp,uj',
+            'HOST': 'localhost',     # Empty for localhost through domain sockets or $
+            'PORT': '',              # Set to empty string for default.
+        }
     }
-}
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': PROJECT_DIR.child('db.sqlite3'),
+#     }
+# }
 
