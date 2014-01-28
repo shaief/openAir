448f33cad6b091906f2e93829f33b92676219e37
shaief <shaief@gmail.com>
2014-01-28 18:07:39 +0200

    added Sentry

--- openair/settings/dokku.py
+++ openair/settings/dokku.py
@@ -12,3 +12,12 @@ SECRET_KEY = environ.get('SECRET_KEY')
 DATABASES = {
 	'default': dj_database_url.config()
 }
+
+
+RAVEN_CONFIG = {
+    'dsn': environ.get('SENTRY_DSN'),
+}
+
+INSTALLED_APPS += (
+    'raven.contrib.django.raven_compat',
+)
