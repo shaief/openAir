ca92205e1b0119b23e900096a7ebba372516475a
Yuval Adam <yuv.adm@gmail.com>
2014-01-28 18:30:19 +0200

    Set static file serving

--- openair/urls.py
+++ openair/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+
 
 admin.autodiscover()   
 
@@ -7,3 +9,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('openair.records.urls')),
 )
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
