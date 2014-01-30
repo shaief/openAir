136aacbf50186860fb4c21a820daaf8cf4fb3bc6
shaief <shaief@gmail.com>
2014-01-31 00:22:33 +0200

    formatting dates - hebrew titles

--- openair/records/views.py
+++ openair/records/views.py
@@ -37,6 +37,7 @@ def parameter_json(request, abbr):
                 [record['name'] for record in records]:
             records.append(dict(name=r.station.name,
                                 zone=r.station.zone.name,
+                                timestamp=(r.timestamp).strftime("%Y-%m-%d %H:%M"),
                                 zone_id=r.station.zone.url_id,
                                 value=r.value))
 
