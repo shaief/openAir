6ad982a4c4ec0129bf8a9b94f5faa37379138a04
shaief <shaief@gmail.com>
2014-02-04 12:54:17 +0200

    additional changes, trying to solve conflicts

--- openair/records/views.py
+++ openair/records/views.py
@@ -10,6 +10,7 @@ import itertools
 
 from django.db.models import Avg
 
+
 def parameters(request):
     latest_record_list = Record.objects.all().order_by('station')[:10]
     context = {'latest_record_list': latest_record_list}
@@ -20,7 +21,8 @@ def parameter(request, abbr):
     parameter_list = Parameter.objects.all()
     p = get_object_or_404(Parameter, abbr=abbr)
     lastupdate = p.record_set.latest('id').timestamp
-    context = dict(parameter=p, parameter_list=parameter_list, lastupdate=lastupdate)
+    context = dict(parameter=p, parameter_list=parameter_list,
+                    lastupdate=lastupdate)
     return render(request, 'records/parameter.html', context)
 
 
@@ -37,13 +39,13 @@ def parameter_json(request, abbr):
         if not r.station.name in \
                 [record['name'] for record in records]:
             records.append(dict(name=r.station.name,
-                                zone=r.station.zone.name,
-                                #station_url=()
-                                datestamp = (r.timestamp).strftime("%d-%m-%Y"),
-                                timestamp=(r.timestamp).strftime("%H:%M"),
-                                datetimestamp=(r.timestamp).strftime("%H:%M %d-%m-%Y"),
-                                zone_id=r.station.zone.url_id,
-                                value=r.value))
+                        zone=r.station.zone.name,
+                        #station_url=()
+                        datestamp=(r.timestamp).strftime("%d-%m-%Y"),
+                        timestamp=(r.timestamp).strftime("%H:%M"),
+                        datetimestamp=(r.timestamp).strftime("%H:%M %d-%m-%Y"),
+                        zone_id=r.station.zone.url_id,
+                        value=r.value))
 
             if not r.station.zone.url_id in \
                     [z['zone_id'] for z in zones]:
@@ -83,49 +85,59 @@ def parameter_json(request, abbr):
 
     return HttpResponse(json.dumps(data))
 
+
 def map(request):
 
     return render(request, 'records/map.html')
+
 
 def stationparams(request, url_id):
     s = get_object_or_404(Station, url_id=url_id)
     station_list = Station.objects.all().order_by('name')
     parameter_list = s.record_set.all()
-    station_params = Parameter.objects.filter(record__station__url_id=url_id).distinct()
+    station_params = Parameter.objects. \
+        filter(record__station__url_id=url_id).distinct()
     lastupdate = s.record_set.latest('id').timestamp
-    context = dict(station=s, station_list=station_list, 
+    context = dict(station=s, station_list=station_list,
         parameter_list=parameter_list, lastupdate=lastupdate,
         station_params=station_params)
     return render(request, 'records/stationparams.html', context)
 
+
 def stationmap(request, url_id):
     s = get_object_or_404(Station, url_id=url_id)
     station_list = Station.objects.all().order_by('name')
-    station_params = Parameter.objects.filter(record__station__url_id=url_id).distinct()
+    station_params = Parameter.objects.\
+        filter(record__station__url_id=url_id).distinct()
     lastupdate = s.record_set.latest('id').timestamp
-    context = dict(station=s, station_list=station_list, 
+    context = dict(station=s, station_list=station_list,
         station_params=station_params, lastupdate=lastupdate)
     return render(request, 'records/stationmap.html', context)
+
 
 def stationmapparam(request, url_id, abbr):
     s = get_object_or_404(Station, url_id=url_id)
     station_list = Station.objects.all().order_by('name')
     lastupdate = s.record_set.latest('id').timestamp
     abbr_id = Parameter.objects.get(abbr=abbr).id
-    total_average_value = Record.objects.filter(parameter=abbr_id).aggregate(Avg('value'))
-    station_params = Parameter.objects.filter(record__station__url_id=url_id).distinct()
+    total_average_value = Record.objects.\
+        filter(parameter=abbr_id).aggregate(Avg('value'))
+    station_params = Parameter.objects.\
+        filter(record__station__url_id=url_id).distinct()
     context = dict(station=s, abbr=abbr, station_list=station_list,
         station_params=station_params, lastupdate=lastupdate,
         total_average_value=total_average_value['value__avg'])
     return render(request, 'records/stationmapparam.html', context)
 
+
 def stationmapwind(request, zone_url_id, station_url_id):
     s = get_object_or_404(Station, url_id=station_url_id)
     station_list = Station.objects.all().order_by('name')
-    context = dict(station=s, abbr='WD', station_list=station_list, \
-                   zone_url_id=int(zone_url_id),\
+    context = dict(station=s, abbr='WD', station_list=station_list,
+                   zone_url_id=int(zone_url_id),
                    lat=s.lat, lon=s.lon)
     return render(request, 'records/stationmapwind_pi.html', context)
+
 
 def stationmap_json(request, url_id):
     s = get_object_or_404(Station, url_id=url_id)
@@ -140,11 +152,13 @@ def stationmap_json(request, url_id):
                                 ))
     geom = [s.lon, s.lat]
 
-    info = dict(records=records, geom=geom, timestamp=(r.timestamp).strftime("%H:%M %d-%m-%Y"))
+    info = dict(records=records, geom=geom,
+        timestamp=(r.timestamp).strftime("%H:%M %d-%m-%Y"))
 
     data = dict(params=params, values=values,
                 geom=geom, timestamp=str(r.timestamp))
     return HttpResponse(json.dumps(info))
+
 
 def stationmap_param_json(request, url_id, abbr):
     s = get_object_or_404(Station, url_id=url_id)
@@ -159,8 +173,9 @@ def stationmap_param_json(request, url_id, abbr):
             records.append(dict(value=r.value,
                                 timestamp=r.timestamp.isoformat()))
     average_value = sum_values / number_of_values
-    data = dict(point=point, records=records, average_value=average_value)                       
+    data = dict(point=point, records=records, average_value=average_value)
     return HttpResponse(json.dumps(data))
+
 
 def stationmapwind_json(request, url_id):
     s = get_object_or_404(Station, url_id=url_id)
@@ -170,7 +185,7 @@ def stationmapwind_json(request, url_id):
     records = list(s.record_set.all().order_by('timestamp')[:24])
     l = itertools.groupby(records, lambda x: x.timestamp)
     for ts, records in l:
-        i+=1
+        i += 1
         params = {x.parameter.abbr: x.value for x in records}
         d = {
              'id': i,
@@ -179,28 +194,29 @@ def stationmapwind_json(request, url_id):
              'timestamp': unicode(ts),
              }
         rv.append(d)
-    data = dict(point=point, records=rv)    
+    data = dict(point=point, records=rv)
     return HttpResponse(json.dumps(data))
+
 
 def stations_json(request):
     s = Station.objects.all().order_by('name')
     stations = []
     for sta in s:
         if not((sta.lon is None) & (sta.lat is None)):
-            if not((sta.lon==0.0) & (sta.lat==0.0)):
-                type="Feature"
-                properties=dict(name=sta.name, url_id=sta.url_id,
-                                 zone=sta.zone.name, zone_url_id=sta.zone.url_id,
-                                 location=sta.location,
-                                )
-                geometry=dict(type="Point",coordinates=[sta.lon, sta.lat])
+            if not((sta.lon == 0.0) & (sta.lat == 0.0)):
+                type = "Feature"
+                properties = dict(name=sta.name, url_id=sta.url_id,
+                         zone=sta.zone.name, zone_url_id=sta.zone.url_id,
+                         location=sta.location,
+                        )
+                geometry = dict(type="Point", coordinates=[sta.lon, sta.lat])
                 stations.append(dict(type=type, properties=properties,
-                                 geometry=geometry,
-                                 name=sta.name, url_id=sta.url_id,
-                                 zone=sta.zone.name, zone_url_id=sta.zone.url_id,
-                                 location=sta.location,
-                                ))                
-    data = dict(type="FeatureCollection",features=stations)                       
+                             geometry=geometry,
+                             name=sta.name, url_id=sta.url_id,
+                             zone=sta.zone.name, zone_url_id=sta.zone.url_id,
+                             location=sta.location,
+                            ))
+    data = dict(type="FeatureCollection", features=stations)
     return HttpResponse(json.dumps(data))
 
 
@@ -214,71 +230,3 @@ def zones(request):
     latest_zone_list = Zone.objects.all().order_by('name')
     context = {'latest_zone_list': latest_zone_list}
     return render(request, 'records/zones.html', context)
-
-'''
-TODO: create a view that passes data from DB to json
-according to query/view. This JSON will be parsed later to
-a beautiful D3 visualization.
-
-def my_ajax_view(request):
-    if not request.is_ajax():
-        raise Http404
-    data_dict = getmydata() #let's suppose is a dict
-    return HttpResponse(json.dumps(data_dict))
-'''
-
-
-def station(request, station_id, start, end):
-    # accessible over: station-1-2013-to-2014
-    start = datetime.date(2013, 1, 1)
-    end = datetime.date(2014, 1, 1)
-    wd = Parameter.objects.first()
-    wd_rec = get_param_time_range(wd, start, end)
-    tstamps, measurements = zip(*[(x.timestamp, x.value) for x in wd_rec])
-    measurements = '[' + ', '.join([str(x) for x in measurements]) + ']'
-    context = {"measurements": measurements}
-    return render(request, 'records/station_view.html', context)
-
-
-def demo_linechart(request, station_id, start, end):
-    """
-    lineChart page
-    """
-    start_time = int(time.mktime(datetime.datetime(2012, 6, 1).timetuple())
-                     * 1000)
-    nb_element = 150
-    xdata = range(nb_element)
-    xdata = map(lambda x: start_time + x * 1000000000, xdata)
-    ydata = [i + random.randint(1, 10) for i in range(nb_element)]
-    ydata2 = map(lambda x: x * 2, ydata)
-
-    tooltip_date = "%d %b %Y %H:%M:%S %p"
-    extra_serie1 = {
-        "tooltip": {"y_start": "", "y_end": " cal"},
-        "date_format": tooltip_date,
-        'color': '#a4c639'
-    }
-    extra_serie2 = {
-        "tooltip": {"y_start": "", "y_end": " cal"},
-        "date_format": tooltip_date,
-        'color': '#FF8aF8'
-    }
-    chartdata = {'x': xdata,
-                 'name1': 'series 1', 'y1': ydata, 'extra1': extra_serie1,
-                 'name2': 'series 2', 'y2': ydata2, 'extra2': extra_serie2}
-
-    charttype = "lineChart"
-    chartcontainer = 'linechart_container'  # container name
-    data = {
-        'charttype': charttype,
-        'chartdata': chartdata,
-        'chartcontainer': chartcontainer,
-        'extra': {
-            'x_is_date': True,
-            'x_axis_format': '%d %b %Y %H',
-            'tag_script_js': True,
-            'jquery_on_ready': False,
-        }
-    }
-    return render(request, 'records/station_view.html', data)
-    # return render_to_response('linechart.html', data)
--- openair/settings/local.py
+++ openair/settings/local.py
@@ -6,10 +6,10 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': PROJECT_DIR.child('db.sqlite3'),
-#     }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': PROJECT_DIR.child('db.sqlite3'),
+     }
+}
 
